bff4ac7d7db49330686d6f2f56edb413ec708662
manweile <manweiler@ualberta.ca>
2024-02-17 14:23:33 -0700

    updates

--- BTFMI.ino
+++ BTFMI.ino
@@ -20,33 +20,35 @@
 
 //arduino libraries
 #include <ArduinoSTL.h>
+#include <algorithm>
+#include <vector>
 #include <SPI.h>
+#include <Wire.h>
 
 //third party function libraries
 #include <PrintEx.h>
-#include <algorithm>
-#include <vector>
 
 //third party hardware libraries
 #include <Adafruit_Si4713.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <Adafruit_FT6206.h>
-#include <Wire.h>
+
+// communications definitions
+#define SERIAL_BAUD 115200		// use default upload speed for convenience
+#define RN52_BAUD 38400			// per BAL docs, best baud rate for RN52
 
 //fm definitions
-#define RESETPIN 22				//pin to use on mega 2560
+#define RESETPIN 22				// pin to use on mega 2560 for Si4713
 #define FMSTATION 8770			// default station 8770 == 87.70 MHz
-#define BAUD 115200				// use default upload speed for convenience
 #define MAX_FREQ 10790			// upper end of FM band
 #define MIN_FREQ 8770			// lower end of FM band
 #define TX_POWER 115			// dBuV, 88-115 max
 #define BROADCAST_LEVEL 60		// noise level for broadcasting stations
 
-//fm debugging flags
+//normal operation & debugging flags
 #define USE_AVAILABLE false
-#define SHOW_SCANNED true
-#define DEBUG_ON false
+#define DEBUG_ON true
 
 //capacitive touch definitions
 #define SD_CS 4
@@ -58,22 +60,25 @@
 #define FRAME_W 100
 #define FRAME_H 50
 
-#define REDBUTTON_X FRAME_X
-#define REDBUTTON_Y FRAME_Y
-#define REDBUTTON_W (FRAME_W/2)
-#define REDBUTTON_H FRAME_H
-
-#define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
-#define GREENBUTTON_Y FRAME_Y
-#define GREENBUTTON_W (FRAME_W/2)
-#define GREENBUTTON_H FRAME_H
+// @TODO just for reference now, delete once direct control buttons are set up
+//#define REDBUTTON_X FRAME_X
+//#define REDBUTTON_Y FRAME_Y
+//#define REDBUTTON_W (FRAME_W/2)
+//#define REDBUTTON_H FRAME_H
+//
+//#define GREENBUTTON_X (REDBUTTON_X + REDBUTTON_W)
+//#define GREENBUTTON_Y FRAME_Y
+//#define GREENBUTTON_W (FRAME_W/2)
+//#define GREENBUTTON_H FRAME_H
 
 Adafruit_Si4713 radio = Adafruit_Si4713(RESETPIN);
 
 Adafruit_FT6206 ts = Adafruit_FT6206();
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-boolean recordOn = false;
+// @TODO just for reference now, delete once direct control buttons are set up
+//boolean recordOn = false;
+
 uint16_t prevFreq;
 uint8_t prevdBuV, prevAntCap, prevNoiseLevel, prevASQ;
 int8_t prevInLevel;
@@ -82,7 +87,7 @@ void setup() {
 	int freq;
 	int station = FMSTATION;
 
-	Serial.begin(BAUD);
+	Serial.begin(SERIAL_BAUD);
 
 	// start the cap screen
 	tft.begin();
@@ -100,101 +105,64 @@ void setup() {
 		tft.setCursor(0, 0);
 		tft.setTextColor(ILI9341_WHITE);
 		tft.setTextSize(3);
-
-		// now we can use tft.print and println
-		Serial.println("GAM Radio");
-		tft.println("GAM Radio");
 	}
 
-    // reset for screen space saving
+	// Banner text
+	tft.println("GAM Radio");
+
+	// reset for screen space saving
 	tft.setTextSize(1);
 
 	// begin with address 0x63 (CS high default)
 	if (!radio.begin()) {
-		Serial.print("Couldn't find Si4713 transceiver");
 		tft.println("Couldn't find Si4713 transceiver");
 		delay(500);
 		exit(2);
 	}
 
-	Serial.println("Scanning for broadcasting stations ...");
-	Serial.println();
-
-	tft.println("Scanning for broadcasting stations ...");
-	tft.println();
-
-	for (freq = MIN_FREQ; freq <= MAX_FREQ; freq += 20) {
-		radio.readTuneMeasure(freq);
-		radio.readTuneStatus();
-		if( radio.currNoiseLevel >= BROADCAST_LEVEL) {
-			Serial.print("Broadcast at "); Serial.print(freq/100.00); Serial.print(" Mhz, Current Noise Level: "); Serial.print(radio.currNoiseLevel);
-			Serial.println();
-
-			tft.print("Broadcast at "); tft.print(freq/100.00); tft.print(" Mhz, Current Noise Level: "); tft.print(radio.currNoiseLevel);
-			tft.println();
+	if(DEBUG_ON) {
+		Serial.println("Scanning for broadcasting stations ...");
+		for (freq = MIN_FREQ; freq <= MAX_FREQ; freq += 20) {
+			radio.readTuneMeasure(freq);
+			radio.readTuneStatus();
+			if( radio.currNoiseLevel >= BROADCAST_LEVEL) {
+				Serial.print("Broadcast at "); Serial.print(freq/100.00); Serial.print(" Mhz, Current Noise Level: "); Serial.print(radio.currNoiseLevel);
+				Serial.println();
+			}
 		}
 	}
 
-	//
+	// normal operation
 	if(USE_AVAILABLE) {
-		station = availableChannels(BROADCAST_LEVEL, FMSTATION, MIN_FREQ, MAX_FREQ, SHOW_SCANNED);
+		// @TODO setup scan frequencies button
 
-		Serial.print("\nTuning into frequency "); Serial.print(station/100.00); Serial.print(" Mhz");
-		Serial.println();
+		// @TODO offset text to right for button before printing following
+		tft.println("Scanning for available frequencies ...");
+		station = availableChannels(BROADCAST_LEVEL, FMSTATION, MIN_FREQ, MAX_FREQ);
 
+		// @TODO clear "Scanning for available frequencies ... " text before printing following
 		tft.print("\nTuning into frequency "); tft.print(station/100.00); tft.print(" Mhz");
 		tft.println();
 	}
 	else {
-		Serial.print("\nTuning into debugging frequency "); Serial.print(FMSTATION/100.00); Serial.print(" Mhz");
-		Serial.println();
-
-		tft.print("\nTuning into debugging frequency "); tft.print(FMSTATION/100.00); tft.print(" Mhz");
+		// scan button non-extant if USE_AVAILABE is false and no need to offset text
+		tft.print("\nTuning into default frequency "); tft.print(FMSTATION/100.00); tft.print(" Mhz");
 		tft.println();
 	}
 
 	/*
-	one of readTuneMeasure(freq) or readTuneStatus() in avalaibleChannels
-	is resetting transmission power to zero
+	readTuneMeasure(freq) in avalaibleChannels enters receive mode (disables transmitter output power)
 	so have to call setTxPower after avalaibleChannels called
-	suspect its readTuneMeasure because:
-	void Adafruit_Si4713::readTuneMeasure(uint16_t freq) {
-		// check freq is multiple of 50khz
-		if (freq % 5 != 0) {
-			freq -= (freq % 5);
-		}
-		//Serial.print("Measuring "); Serial.println(freq);
-		_i2ccommand[0] = SI4710_CMD_TX_TUNE_MEASURE;
-		_i2ccommand[1] = 0;
-		_i2ccommand[2] = freq >> 8;
-		_i2ccommand[3] = freq;
-		_i2ccommand[4] = 0;
-
-		sendCommand(5);
-		while (getStatus() != 0x81) delay(10);
-	}
-	_i2ccommand[4], which is antenna capacitance, is being reset to zero
 	*/
-
 	radio.setTXpower(TX_POWER);
 	radio.tuneFM(station);
 
-	Serial.print("Setting TX power: "); Serial.print(TX_POWER); Serial.print(" dBuV");
-	Serial.println();
-
 	// This will tell you the status in case you want to read it from the chip
 	radio.readTuneStatus();
-
-	Serial.print("\nFrequency: "); Serial.print(radio.currFreq/100.00);
-
-	Serial.print("\nFrequency dBuV: "); Serial.print(radio.currdBuV);
-
-	Serial.print("\nANT capacitance: "); Serial.print(radio.currAntCap);
-	Serial.println("\n");
-
+	prevAntCap = radio.currAntCap;
+	// @TODO prob don't need prevFreq and prevdBuv
 	prevFreq = radio.currFreq/100.00;
 	prevdBuV = radio.currdBuV;
-	prevAntCap = radio.currAntCap;
 
 	radio.readASQ();
 	prevASQ = radio.currASQ;
@@ -203,67 +171,73 @@ void setup() {
 	// set GP1 and GP2 to output
 	//radio.setGPIOctrl(_BV(1) | _BV(2));
 
-	//tft.fillScreen(ILI9341_BLACK);
-	// origin = left,top landscape (USB left upper)
-	//tft.setRotation(1);
+	// @TODO set up buttons
+	//			vol up
+	//	prev	play/pause	next
+	//			vol dn
+
+	// @TODO just for reference now, delete once direct control buttons are set up
 	// redBtn();
 }
 
 void loop() {
-	// react to cap screen use input actions
-	if (ts.touched())
-	{
-		// Retrieve a point
-		TS_Point p = ts.getPoint();
+	//// react to cap screen use input actions
+	//if (ts.touched())
+	//{
+		//// Retrieve a point
+		//TS_Point p = ts.getPoint();
+//
+		//// rotate coordinate system & flip it around to match the screen.
+		//p.x = map(p.x, 0, 240, 240, 0);
+		//p.y = map(p.y, 0, 320, 320, 0);
+		//int y = tft.height() - p.x;
+		//int x = p.y;
+//
+		//// @TODO just for reference now, delete once direct control buttons are set up
+		////if (recordOn)
+		////{
+			////if((x > REDBUTTON_X) && (x < (REDBUTTON_X + REDBUTTON_W))) {
+				////if ((y > REDBUTTON_Y) && (y <= (REDBUTTON_Y + REDBUTTON_H))) {
+					//////Serial.print("Red btn hit\n");
+					////redBtn();
+				////}
+			////}
+		////}
+		////else //Record is off (recordOn == false)
+		////{
+			////if((x > GREENBUTTON_X) && (x < (GREENBUTTON_X + GREENBUTTON_W))) {
+				////if ((y > GREENBUTTON_Y) && (y <= (GREENBUTTON_Y + GREENBUTTON_H))) {
+					//////Serial.print("Green btn hit\n");
+					////greenBtn();
+				////}
+			////}
+		////}
+	//}
 
-		// rotate coordinate system & flip it around to match the screen.
-		p.x = map(p.x, 0, 240, 240, 0);
-		p.y = map(p.y, 0, 320, 320, 0);
-		int y = tft.height() - p.x;
-		int x = p.y;
+	if(DEBUG_ON) {
+		// for changes in antenna capacitance
+		radio.readTuneStatus();
 
-		//if (recordOn)
-		//{
-			//if((x > REDBUTTON_X) && (x < (REDBUTTON_X + REDBUTTON_W))) {
-				//if ((y > REDBUTTON_Y) && (y <= (REDBUTTON_Y + REDBUTTON_H))) {
-					////Serial.print("Red btn hit\n");
-					//redBtn();
-				//}
-			//}
-		//}
-		//else //Record is off (recordOn == false)
-		//{
-			//if((x > GREENBUTTON_X) && (x < (GREENBUTTON_X + GREENBUTTON_W))) {
-				//if ((y > GREENBUTTON_Y) && (y <= (GREENBUTTON_Y + GREENBUTTON_H))) {
-					////Serial.print("Green btn hit\n");
-					//greenBtn();
-				//}
-			//}
-		//}
-	}
+		if( radio.currAntCap != prevAntCap ) {
+			Serial.print("Curr ANT capacitance: "); Serial.print(radio.currAntCap);
+			Serial.println();
+			prevAntCap = radio.currAntCap;
+		}
 
+		// for changes in ASQ and InLevel
+		radio.readASQ();
+		if( radio.currASQ != prevASQ ) {
+			Serial.print("Curr ASQ: 0x"); Serial.print(radio.currASQ, HEX); Serial.print("\tCurr InLevel: "); Serial.print(radio.currInLevel);
+			Serial.println();
+			prevASQ = radio.currASQ;
+		}
 
-	radio.readTuneStatus();
-	// I only want to see CHANGES in antenna capacitance and only for debugging purposes
-	if( radio.currAntCap != prevAntCap ) {
-		Serial.print("Curr ANT capacitance: "); Serial.print(radio.currAntCap);
-		Serial.println();
-		prevAntCap = radio.currAntCap;
-	}
-
-	radio.readASQ();
-	// I only want to see CHANGES in ASQ and InLevel and only for debugging purposes
-	if( radio.currASQ != prevASQ ) {
-		Serial.print("Curr ASQ: 0x"); Serial.print(radio.currASQ, HEX); Serial.print("\tCurr InLevel: "); Serial.print(radio.currInLevel);
-		Serial.println();
-		prevASQ = radio.currASQ;
-	}
-
-	// currInLevel changes too often by too little, so need a floor delta value
-	if( abs(radio.currInLevel - prevInLevel) > 10 ) {
-		Serial.print("Curr ASQ: 0x"); Serial.print(radio.currASQ, HEX); Serial.print("\tCurr InLevel: "); Serial.print(radio.currInLevel);
-		Serial.println();
-		prevInLevel = radio.currInLevel;
+		// currInLevel changes too often by too little, so need a floor delta value
+		if( abs(radio.currInLevel - prevInLevel) > 10 ) {
+			Serial.print("Curr ASQ: 0x"); Serial.print(radio.currASQ, HEX); Serial.print("\tCurr InLevel: "); Serial.print(radio.currInLevel);
+			Serial.println();
+			prevInLevel = radio.currInLevel;
+		}
 	}
 
 	// @TODO if ASQ over modulating (confirm this value in docs), reduce rn-52 output volume 1 step and print to screen
@@ -288,10 +262,9 @@ void loop() {
 * @param{Int} defBroadcast The defualt frequency
 * @param{Int} loEnd The low end of the FM band
 * @param{Int} hiEnd The high end of the FM band
-* @param{Boolean} showinfo Flag to print information
 * @return{Int} newBroadcast The new frequency to broadcast on
 */
-int availableChannels(int maxLevel, int defBroadcast, int loEnd, int hiEnd, boolean showInfo)
+int availableChannels(int maxLevel, int defBroadcast, int loEnd, int hiEnd)
 {
 	int freq;
 	int newBroadcast;
@@ -308,16 +281,12 @@ int availableChannels(int maxLevel, int defBroadcast, int loEnd, int hiEnd, bool
 	newBroadcast = defBroadcast;
 
 	//scan the fm band from loEnd to hiEnd in .2 Mhz increments, save frequencies with low enough noise level
-	// Serial print only cause I don't want to clutter up tft screen
-	Serial.print("\nScanning for available frequencies ...");
-	Serial.println();
-
 	for (freq = loEnd; freq <= hiEnd; freq += 20) {
 		radio.readTuneMeasure(freq);
 		radio.readTuneStatus();
 
 		if( radio.currNoiseLevel < maxLevel) {
-			if (showInfo){
+			if (DEBUG_ON){
 				Serial.print("Available frequency "); Serial.print(freq/100.00); Serial.print(" Mhz, Noise Level: "); Serial.print(radio.currNoiseLevel);
 				Serial.println();
 			}
@@ -329,21 +298,22 @@ int availableChannels(int maxLevel, int defBroadcast, int loEnd, int hiEnd, bool
 	sort(scannedFreqs.begin(), scannedFreqs.end(), sort_pred());
 	newBroadcast = scannedFreqs[0].first;
 
-	Serial.print("\nSorted available frequencies\n");
-	// reverse print the sorted scanned frequencies because I want the quietest frequency to be last datum printed
-	for(int i = scannedFreqs.size() -1; i >= 0; i--) {
-		if(showInfo){
+	if(DEBUG_ON) {
+		Serial.print("\nSorted available frequencies\n");
+
+		// reverse print the sorted scanned frequencies because I want the quietest frequency to be last datum printed
+		for(int i = scannedFreqs.size() -1; i >= 0; i--) {
 			Serial.print("Frequency "); Serial.print(scannedFreqs[i].first/100.00); Serial.print(" Mhz, Noise Level: "); Serial.print(scannedFreqs[i].second);
 			Serial.println();
 		}
+
+		// display new frequency info
+		Serial.print("\nFound "); Serial.print(scannedFreqs.size()); Serial.print(" frequencies with noise less than "); Serial.print(maxLevel);
+		Serial.println();
+
+		Serial.print("Quietest frequency: "); Serial.print(newBroadcast/100.00); Serial.print(" Mhz, Noise Level: "); Serial.print(scannedFreqs[0].second);
+		Serial.println();
 	}
-
-	// display new frequency
-	Serial.print("\nFound "); Serial.print(scannedFreqs.size()); Serial.print(" frequencies with noise less than "); Serial.print(maxLevel);
-	Serial.println();
-
-	Serial.print("Quietest frequency: "); Serial.print(newBroadcast/100.00); Serial.print(" Mhz, Noise Level: "); Serial.print(scannedFreqs[0].second);
-	Serial.println();
 
 	return newBroadcast;
 }
@@ -352,38 +322,40 @@ int availableChannels(int maxLevel, int defBroadcast, int loEnd, int hiEnd, bool
 * Capacitive Touch Functions
 */
 
+// @TODO function header
 void drawFrame()
 {
 	tft.drawRect(FRAME_X, FRAME_Y, FRAME_W, FRAME_H, ILI9341_BLACK);
 }
 
 
-// @TODO function header
-void redBtn()
-{
-	tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_RED);
-	tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_BLUE);
-	drawFrame();
-	tft.setCursor(GREENBUTTON_X + 6 , GREENBUTTON_Y + (GREENBUTTON_H/2));
-	tft.setTextColor(ILI9341_WHITE);
-	tft.setTextSize(1);
-	tft.println("ON");
-	recordOn = false;
-}
+// @TODO just for reference now, delete once direct control buttons are set up
+//void redBtn()
+//{
+	//tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_RED);
+	//tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_BLUE);
+	//drawFrame();
+	//tft.setCursor(GREENBUTTON_X + 6 , GREENBUTTON_Y + (GREENBUTTON_H/2));
+	//tft.setTextColor(ILI9341_WHITE);
+	//tft.setTextSize(1);
+	//tft.println("ON");
+	//recordOn = false;
+//}
 
-// @TODO function header
-void greenBtn()
-{
-	tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_GREEN);
-	tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_BLUE);
-	drawFrame();
-	tft.setCursor(REDBUTTON_X + 6 , REDBUTTON_Y + (REDBUTTON_H/2));
-	tft.setTextColor(ILI9341_WHITE);
-	tft.setTextSize(1);
-	tft.println("OFF");
-	recordOn = true;
-}
+// @TODO just for reference now, delete once direct control buttons are set up
+//void greenBtn()
+//{
+	//tft.fillRect(GREENBUTTON_X, GREENBUTTON_Y, GREENBUTTON_W, GREENBUTTON_H, ILI9341_GREEN);
+	//tft.fillRect(REDBUTTON_X, REDBUTTON_Y, REDBUTTON_W, REDBUTTON_H, ILI9341_BLUE);
+	//drawFrame();
+	//tft.setCursor(REDBUTTON_X + 6 , REDBUTTON_Y + (REDBUTTON_H/2));
+	//tft.setTextColor(ILI9341_WHITE);
+	//tft.setTextSize(1);
+	//tft.println("OFF");
+	//recordOn = true;
+//}
 
+//@TODO not sure I need this anymore
 /**
 * Prints supplied text to touch screen
 * The input string is expected to be fully formatted by calling function
@@ -400,6 +372,7 @@ void tftPrint(String inputText)
 	tft.print(inputText);
 }
 
+//@TODO not sure I need this anymore
 /**
 * Prints text to Serial monitor
 * The input string is expected to be fully formatted by calling function
